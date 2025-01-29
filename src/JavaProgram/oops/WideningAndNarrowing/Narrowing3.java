65dcb4e4e88917294e0f1f0e6677f58f6d94ab74
swati yadav <swatiyadav6872@gmail.com>
2025-01-29 16:36:13 +0530

    init commit

---
+++ src/JavaProgram/oops/WideningAndNarrowing/Narrowing3.java
@@ -0,0 +1,10 @@
+package JavaProgram.oops.WideningAndNarrowing;
+
+public class Narrowing3 {
+    public static void main(String[] args) {
+        char c='a';
+        int  i=(int) c;
+        System.out.println(c);
+        System.out.println(i);
+    }
+}
---
+++ src/JavaProgram/oops/WideningAndNarrowing/Narrowing6.java
@@ -0,0 +1,10 @@
+package JavaProgram.oops.WideningAndNarrowing;
+
+public class Narrowing6 {
+    public static void main(String[] args) {
+        byte b=56;
+        short s=(short) b;
+        System.out.println(b);
+        System.out.println(s);
+    }
+}
