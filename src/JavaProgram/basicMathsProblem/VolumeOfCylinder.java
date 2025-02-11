6b2e8985a06cd62e09de3aa3998bdfa1e776b41c
swati yadav <swatiyadav6872@gmail.com>
2025-02-11 21:50:14 +0530

    Questions based on basic problem

---
+++ src/JavaProgram/basicMathsProblem/VolumeOfCylinder.java
@@ -0,0 +1,10 @@
+package JavaProgram.basicMathsProblem;
+
+public class VolumeOfCylinder {
+    public static void main(String[] args) {
+        float r=20.5f;
+        float h=25.6f;
+        double area=((22.0/7.0)*r*r*h);
+        System.out.println(area);
+    }
+}
