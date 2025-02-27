7f12abb40c46173b75b73e93481bdd465e9448a0
swati yadav <swatiyadav6872@gmail.com>
2025-02-27 21:58:23 +0530

    logic building question on array

---
+++ src/arrayPrograms/ReverseArray.java
@@ -0,0 +1,22 @@
+package arrayPrograms;
+
+public class ReverseArray {
+   static void reverse(int []arr){
+        int low=0;
+        int high=arr.length-1;
+        while (low<=high){
+            int temp=arr[low];
+            arr[low]=arr[high];
+            arr[high]=temp;
+            low++;
+            high--;
+        }
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5};
+        reverse(arr);
+    }
+}
